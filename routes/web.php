de721e621636add7e30f1deba9fd0e974bbf25e6
Arya Veda Setyanindito <vedakhansa@gmail.com>
2023-07-08 00:09:45 +0700

    Create UserController and modification route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/users/{user}/edit', [UserController::class,'edit']);
+Route::put('/users/{user}', [UserController::class,'update']);
+Route::delete('/users/{user}', [UserController::class,'destroy']);
